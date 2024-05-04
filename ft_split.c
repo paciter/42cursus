c0c739b4917dc8238994246ac0b2b1a61317172b
paciter <167830421+paciter@users.noreply.github.com>
2024-05-04 03:26:40 +0200

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -12,61 +12,72 @@
 
 #include "libft.h"
 
-static size_t	count_words(char const *s, char c)
+static size_t    count_words(char const *s, char c)
 {
-	size_t	words;
-	size_t	i;
+    size_t    words;
+    size_t    i;
 
-	words = 0;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
-			words++;
-		i++;
-	}
-	return (words);
+    words = 0;
+    i = 0;
+    if (s == NULL || *s == '\0') // Vérification de la chaîne vide ou nulle
+        return 0;
+    while (s[i])
+    {
+        if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+            words++;
+        i++;
+    }
+    return (words);
 }
 
-static size_t lenword(const char *s, char c)
+ static size_t lenword(const char *s, char c)
 {
-	size_t	i;
-	size_t	j;
-	
-	i = 0;
-	j = 0;
-	while(s[i] == c)
-	i++;
-	while (s[i] != c && s[i] != '\0')
-	{
-		j++;
-		i++;
-	}
-	return (j);
+    size_t    i;
+    size_t    j;
+    
+    i = 0;
+    j = 0;
+    while(s[i] == c)
+    i++;
+    while (s[i] != c && s[i] != '\0')
+    {
+        j++;
+        i++;
+    }
+    return (j);
 }
 char **ft_split(char const *s, char c)
 {
-	char	**ptr;
-	size_t		i;
-	size_t		j;
-	size_t	wordnumber;
-	size_t	lenmotact;
-	
-	wordnumber = count_words(s,c);
-	i = 0;
-	j = 0;
+    char    **ptr;
+    size_t        i;
+    size_t        j;
+    size_t    wordnumber;
+    size_t    lenmotact;
+    
+    wordnumber = count_words(s,c);
+    i = 0;
+    j = 0;
 
-	ptr = (char **)malloc(sizeof(char *) * (wordnumber + 1));
-	if (!ptr)
-		return (NULL);
-	while ( i < wordnumber && s[j])
-	{
-		while (s[j] == c)
-			j++;
-		lenmotact = lenword(&s[i], c);
-		ptr[i] = ft_substr(&s[j], 0, lenmotact);
-        	if (!ptr[i])
-            		return NULL;
+    ptr = (char **)malloc(sizeof(char *) * (wordnumber + 1));
+    if (!ptr)
+        return (NULL);
+    while ( i < wordnumber && s[j])
+    {
+        while (s[j] == c)
+            j++;
+        lenmotact = lenword(s, c);
+        ptr[i] = ft_substr(s, j, lenmotact);
+            if (!ptr[i])
+            {
+            
+              while (i > 0)
+              {
+                free(ptr[i - 1]);
+                i--;
+              }
+              free(ptr);
+              return (NULL);
+            }
         j += lenmotact;
         i++;
     }
