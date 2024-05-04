6619f732b244e4171e30c900237e38dab8061652
paciter <167830421+paciter@users.noreply.github.com>
2024-05-04 03:23:33 +0200

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -12,6 +12,32 @@
 
 #include "libft.h"
 
+char
+    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    size_t    i;
+    size_t    j;
+    char    *str;
+    
+    if (s == NULL || *s == '\0')
+        return NULL;
+
+    str = (char*)malloc(sizeof(*s) * (len + 1));
+    if (!str)
+        return (NULL);
+    i = start;
+    j = 0;
+    while (s[i] && j < len)
+    {
+       str[j] = s[i];
+       j++;
+       i++;
+    }
+
+    str[j] = '\0';
+    return (str);
+}
+/*
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
@@ -44,25 +70,7 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 	
 	return (ptr);
 }
-/*
-int main(int argc ,char **argv) 
-{
-	if (argc != 4)
-		return (1);
-	size_t	len = (size_t)atoi(argv[3]);
-	unsigned int	start = (unsigned int)atoi(argv[2]);
-	
-	printf("len value %lu | ",len);
-	printf("start value %i \n ",start);
-	
-	char *ptr = ft_substr(argv[1], start, len);
-	
-	printf("len value %lu | ",len);
-	printf("start value %i \n ",start);
-    if (ptr != NULL) {
-        printf("sous chaine %s\n | ",ptr);
-        free(ptr);
-    } else {
+*/
         printf("La sous-chaîne est NULL.\n");
     }
 
