a569179c58cbf8d42f21be30f3db903f36943039
paciter <167830421+paciter@users.noreply.github.com>
2024-05-12 00:16:35 +0200

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,7 +1,7 @@
 #include "libft.h"
 
 size_t ft_strlcpy(char *dest, const char *src, size_t size)
- {
+{
     size_t i; 
 	 
 	i = 0;
@@ -15,13 +15,11 @@ size_t ft_strlcpy(char *dest, const char *src, size_t size)
     }
     if (size > 0)
         dest[i] = '\0';
-	i++;
-
+	    
     while (src[i])
-        i++;
-    return i;
+	i++;
+    return (i);
 }
-
 /*
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
