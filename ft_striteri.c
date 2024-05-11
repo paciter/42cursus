0c1c91d97d44ad62847569ad2572efc665c85b53
paciter <167830421+paciter@users.noreply.github.com>
2024-05-11 20:11:25 +0200

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,17 +1,13 @@
 #include "libft.h"
 
-void    ft_striteri(char *s, void (f)(unsigned int, char *))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-    size_t    i;
-    size_t    len;
+	size_t	i;
 
-    if (!s || !f)
-        return ;
-    len = ft_strlen(s);
-    i = 0;
-    while (i < len)
-    {
-        f(i, &s[i]);
-        i++;
-    }
+	i = 0;
+	while (s[i])
+	{
+		(*f)(i, s + i);
+    	i++;
+	}
 }
