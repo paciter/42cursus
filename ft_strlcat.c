43cb89717eb9ed74f47d3d72f299f190590dd883
paciter <167830421+paciter@users.noreply.github.com>
2024-05-11 23:50:26 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -15,28 +15,25 @@
 size_t ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t i;
-	size_t j;
 	size_t dlen;
 	size_t slen;
 		
 	i = 0;
 	dlen = ft_strlen(dest);
 	slen = ft_strlen(src);
-	j = dlen;
 		
 	if (dlen < size - 1 && size > 0)
 	{
 		while (src[i] && dlen + i < size - 1)
 		{
-			dest[j] = src[i];
-			j++;
+			dest[dlen] = src[i];
+			dlen++;
 			i++;
 		}
-	
-		dest[j] = '\0';
-    	return (dlen + slen);
+		dest[dlen] = '\0';
+    		return (dlen + slen);
 	}
 	else
-		return (dlen +slen);
+		return (dlen + slen);
 	
 }
