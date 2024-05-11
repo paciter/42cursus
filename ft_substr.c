8388d223c834b274e66ecd6a7ae98b41a0762c4e
paciter <167830421+paciter@users.noreply.github.com>
2024-05-11 23:23:05 +0200

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -12,8 +12,7 @@
 
 #include "libft.h"
 
-char
-    *ft_substr(char const *s, unsigned int start, size_t len)
+char   *ft_substr(char const *s, unsigned int start, size_t len)
 {
     size_t    i;
     size_t    j;
@@ -37,31 +36,4 @@ char
     str[j] = '\0';
     return (str);
 }
-/*
-char
-    *ft_substr(char const *s, unsigned int start, size_t len)
-{
-    size_t    i;
-    size_t    j;
-    char    *str;
-    
-    if (s == NULL || *s == '\0')
-        return NULL;
-
-    str = (char*)malloc(sizeof(*s) * (len + 1));
-    if (!str)
-        return (NULL);
-    i = start;
-    j = 0;
-    while (s[i] && j < len)
-    {
-       str[j] = s[i];
-       j++;
-       i++;
-    }
-
-    str[j] = '\0';
-    return (str);
-}
-*/
 
